1fef8e024c0ecd86580b270d4cc2063c7668e5d1
smondalcse <cse.sanat@gmail.com>
2021-12-11 16:27:16 +0600

    Initial commit

---
+++ app/src/main/java/com/nitolniloy/myapplication/ExampleAdapter.java
@@ -0,0 +1,109 @@
+package com.nitolniloy.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<ExampleItem> mExampleList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+        void onAddItemClick(int position);
+        void onRemoveItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public TextView mTextView1;
+        public TextView mTextView2, txt_item_qty;
+        public Button btn_add, btn_remove;
+
+        public ExampleViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+            txt_item_qty = itemView.findViewById(R.id.txt_item_qty);
+            btn_add = itemView.findViewById(R.id.btn_add);
+            btn_remove = itemView.findViewById(R.id.btn_remove);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            btn_add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onAddItemClick(position);
+                            txt_item_qty.setText("Qty: 1");
+                        }
+                    }
+                }
+            });
+
+            btn_remove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onRemoveItemClick(position);
+                            txt_item_qty.setText("Qty: -1");
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public ExampleAdapter(ArrayList<ExampleItem> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_item, parent, false);
+        ExampleViewHolder evh = new ExampleViewHolder(v, mListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+}
---
+++ app/src/main/java/com/nitolniloy/myapplication/MainActivity.java
@@ -0,0 +1,106 @@
+
+
+package com.nitolniloy.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<ExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button buttonInsert;
+    private Button buttonRemove;
+    private EditText editTextInsert;
+    private EditText editTextRemove;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createExampleList();
+        buildRecyclerView();
+
+        buttonInsert = findViewById(R.id.button_insert);
+        buttonRemove = findViewById(R.id.button_remove);
+        editTextInsert = findViewById(R.id.edittext_insert);
+        editTextRemove = findViewById(R.id.edittext_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextInsert.getText().toString());
+                insertItem(position);
+            }
+        });
+
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editTextRemove.getText().toString());
+                removeItem(position);
+            }
+        });
+    }
+
+    public void insertItem(int position) {
+        mExampleList.add(position, new ExampleItem(R.drawable.ic_gesture_24, "New Item At Position" + position, "This is Line 2"));
+        mAdapter.notifyItemInserted(position);
+    }
+
+    public void removeItem(int position) {
+        mExampleList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+    }
+
+    public void createExampleList() {
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new ExampleItem(R.drawable.ic_audiotrack_24, "Line 1", "Line 2"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_gesture_24, "Line 3", "Line 4"));
+        mExampleList.add(new ExampleItem(R.drawable.ic_objects_24, "Line 5", "Line 6"));
+    }
+
+    public void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(getApplicationContext(), "Row", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAddItemClick(int position) {
+                Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_SHORT).show();
+               // insertItem(position);
+            }
+
+            @Override
+            public void onRemoveItemClick(int position) {
+                Toast.makeText(getApplicationContext(), "Remove", Toast.LENGTH_SHORT).show();
+               // removeItem(position);
+            }
+        });
+
+    }
+}
